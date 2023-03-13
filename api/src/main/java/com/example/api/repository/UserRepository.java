5f0d60fc53c83f288af8c3285ee87fd2541e42f5
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 10:17:21 +0000

    Update UserRepository Class

--- api/src/main/java/com/example/api/repository/UserRepository.java
+++ api/src/main/java/com/example/api/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.api.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.api.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
