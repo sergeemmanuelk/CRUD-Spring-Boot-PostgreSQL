6b65fbbdc1585982f0457b49db0e46af98e96234
sergeemmanuelk <serge_em@outlook.fr>
2023-03-13 10:03:25 +0000

    feat: creating a User model

---
+++ api/src/main/java/com/example/api/model/User.java
@@ -0,0 +1,27 @@
+package com.example.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	private String email;
+}
